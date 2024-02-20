64bdd5e4fb94a0b7e974f375879ab795f5c5579c
JaeHong <dnflwoghddl@gmail.com>
2024-02-21 02:11:54 +0900

    useCounter(custom hook) 제작 및 적용

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 // import Library from "./chapter-03/Library";
 // import Clock from "./chapter-04/Clock";
-import CommentList from "./chapter-05/CommentList";
+// import CommentList from "./chapter-05/CommentList";
+import Accommodate from "./chapter-07/Accommodate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>
-);
+root.render(<Accommodate />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
